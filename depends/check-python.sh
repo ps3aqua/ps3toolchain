e90533efa25688f62340ddf543e771db792274a2
Paweł Kołodziejski <aquadran@gmail.com>
2026-03-06 07:02:53 +0100

    check for python3

--- depends/check-python.sh
+++ depends/check-python.sh
@@ -2,10 +2,10 @@
 # check-python.sh by Naomi Peori (naomi@peori.ca)
 
 ## Check for python.
-( python --version || python -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python before continuing."; exit 1; }
+( python3 --version || python3 -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python before continuing."; exit 1; }
 
 ## Check for python-config
-pyprefix=$(python-config --prefix)
+pyprefix=$(python3-config --prefix)
 [ $? -eq 0 ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
 
 ## Check for python header files
